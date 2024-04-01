2f32ad61c1b65259ed0ba9f01a5152f5079b7535
Lavina <150042947+lavinad04@users.noreply.github.com>
2024-04-01 11:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+		getMakeModel(make){
+		return this.make,this.model;
+		}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	this.make = make;
+	this.model = model;
+	this.topSpeed = topSpeed;
+		getTopSpeed(topSpeed){
+		return this.topSpeed;
+		}
+}
 
-// Do not change the code below
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed()); 
 window.Car = Car;
 window.SportsCar = SportsCar;
