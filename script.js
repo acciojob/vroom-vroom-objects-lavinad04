b3c1603c7255453215653cd28df4f47d6b6459e9
Lavina <150042947+lavinad04@users.noreply.github.com>
2024-04-01 11:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
 function Car(make, model) {
 	this.make = make;
 	this.model = model;
-	this.getMakeModel = function(){
-		return this.make,this.model;
-		}
+	this.getMakeModel = function() {
+		return this.make + " " + this.model;
+	};
 }
 
 function SportsCar(make, model, topSpeed) {
 	this.make = make;
 	this.model = model;
 	this.topSpeed = topSpeed;
-	this.getTopSpeed = function(){
+	this.getTopSpeed = function() {
 		return this.topSpeed;
-		}
+	};
 }
 
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 const two = new Car("suite", "bmw");
 console.log(two.getMakeModel()); 
 console.log(car.getTopSpeed()); 
+
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Car(make, model) {
+// 	this.make = make;
+// 	this.model = model;
+// 	this.getMakeModel = function(){
+// 		return this.make+" "+this.model;
+// 		}
+// }
+
+// function SportsCar(make, model, topSpeed) {
+// 	this.make = make;
+// 	this.model = model;
+// 	this.topSpeed = topSpeed;
+// 	this.getTopSpeed = function(){
+// 		return this.topSpeed;
+// 		}
+// }
+
+// const car = new SportsCar("Ferrari", "Testarossa", 200);
+// const two = new Car("suite", "bmw");
+// console.log(two.getMakeModel()); 
+// console.log(car.getTopSpeed()); 
+
+// window.Car = Car;
+// window.SportsCar = SportsCar;
