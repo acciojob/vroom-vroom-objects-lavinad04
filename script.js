c3d125f1caf3c0184c33c783d11c52eb67205cf0
Lavina <150042947+lavinad04@users.noreply.github.com>
2024-04-01 12:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function Car(make, model) {
 	this.make = make;
-	this.model = model;
-	this.getMakeModel = function() {
+	this.model = model;}
+	Car.prototype.getMakeModel = function() {
 		return this.make + " " + this.model;
 	};
-}
+
 
 function SportsCar(make, model, topSpeed) {
 	this.make = make;
 	this.model = model;
-	this.topSpeed = topSpeed;
-	this.getTopSpeed = function() {
-		return this.topSpeed;
-	};
-}
+	this.topSpeed = topSpeed;}
+	SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
 
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel()); 
